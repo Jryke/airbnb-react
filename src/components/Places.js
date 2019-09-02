e58df3a67439551e661c4efb08dec1d1cacede3b
Jryke <rykerson@gmail.com>
2019-09-02 16:46:20 +0700

    Places added to Routes

---
+++ src/components/Places.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Places extends React.Component {
+	render() {
+		return(
+				<h1>Places Test</h1>
+		)
+	}
+}
+
+export default Places
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Places from './Places'
 
 class Routes extends React.Component {
 	render() {
 		return(
 			<BrowserRouter>
 				<Switch>
-
+					<Places />
 				</Switch>
 			</BrowserRouter>
 		)
