813c27f28bd9c56e0e0ca13eee7284c1d0001b12
Jryke <rykerson@gmail.com>
2019-09-03 14:30:41 +0700

    Favorites added to Routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,6 +5,7 @@ import Place from './Place'
 import Confirm from './Confirm'
 import Profile from './Profile'
 import Bookings from './Bookings'
+import Favorites from './Favorites'
 import '../styles/global.css'
 
 class Routes extends React.Component {
@@ -14,6 +15,7 @@ class Routes extends React.Component {
 				<Switch>
 					<Route path='/Profile' component={Profile} />
 					<Route path='/Place' component={Place} />
+					<Route path='/Favorites' component={Favorites} />
 					<Route path='/Confirm' component={Confirm} />
 					<Route path='/Bookings' component={Bookings} />
 					<Route path='/' component={Places} />
