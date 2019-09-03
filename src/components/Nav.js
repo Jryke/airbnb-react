0eccc0935595e47b31d1f0011138802e296cffcf
Jryke <rykerson@gmail.com>
2019-09-03 16:36:59 +0700

    Link to /profile added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,9 +7,9 @@ class Nav extends React.Component {
 	render() {
 		return(
 			<nav>
-				<Link to="/Places" className="logo" style={{backgroundImage: "url('./images/logo-airbnb.png')"}}></Link>
+				<Link to="/places" className="logo" style={{backgroundImage: "url('./images/logo-airbnb.png')"}}></Link>
 				<div className="profile">
-					<Link to="profile.html" className="button">
+					<Link to="/profile" className="button">
 						<div className="avatar" style={{backgroundImage: `url(${this.props.user.avatar})`}}></div>
 						<span>{this.props.user.name}</span>
 					</Link>
