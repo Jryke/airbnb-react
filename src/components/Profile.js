2dbe50550c9b6249a5cff7e31d68396496b21d90
Jryke <rykerson@gmail.com>
2019-09-03 13:02:56 +0700

    Profile created

---
+++ src/components/Profile.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Nav from './Nav'
+import Sidebar from './Sidebar'
+
+class Profile extends React.Component {
+	state = {
+		user: {
+			name: 'Tony',
+			email: 'tony@tortugacoders.com',
+			location: 'Thailand',
+			avatar: 'https://randomuser.me/api/portraits/men/9.jpg'
+		}
+	}
+	render(){
+		return(
+			<>
+				<Nav user={this.state.user}/>
+				<div className="grid medium">
+					<div className="grid sidebar-left">
+						<Sidebar />
+						<div className="content">
+							<h2>My Profile</h2>
+							<form>
+								<div className="group">
+									<label>Name</label>
+									<input type="text" defaultValue={this.state.user.name} />
+								</div>
+								<div className="group">
+									<label>Email</label>
+									<input type="email" value={this.state.user.email} />
+								</div>
+								<div className="group">
+									<label>Location</label>
+									<input type="text" value={this.state.user.location} />
+								</div>
+								<div className="group">
+									<label>Profile Picture</label>
+									<div className="user">
+										<div className="avatar" style={{backgroundImage: `url(${this.state.user.avatar})`}}></div>
+										<div className="name">
+											<input type="file" />
+										</div>
+									</div>
+								</div>
+								<button>Save Changes</button>
+							</form>
+							<hr />
+							<button className="secondary">Logout</button>
+						</div>
+					</div>
+				</div>
+			</>
+		)
+	}
+}
+
+export default Profile
