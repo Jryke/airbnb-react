4084d2122edf3589a9f0e578d9f562652ebf3512
Jryke <rykerson@gmail.com>
2019-09-15 16:24:32 +0700

    120201

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,54 @@
 import React from 'react'
+import axios from 'axios'
 import {Link} from 'react-router-dom'
 import '../styles/buttons.css'
 import '../styles/forms.css'
 
 class Login extends React.Component {
+	state = {
+		email: '',
+		password: '',
+		errorMessage: ''
+	}
+
+	sendInputToState = (e, input) => {
+		let state = this.state
+		state[input] = e.target.value
+		this.setState({state})
+	}
+
+	submitLogin = (e) => {
+		e.preventDefault()
+		if (this.state.email && this.state.password) {
+			axios.post('http//localhost:4000/login', {
+				email: this.state.email,
+				password: this.state.password
+			})
+		} else {
+			this.setState({
+				errorMessage: '*email and password must be completed to log in*'
+			})
+		}
+	}
+
+
 	render() {
 		return(
 			<div className="grid center middle tall image">
 				<div className="card small">
 					<div className="content">
 						<div className="logo" style={{backgroundImage: "url('images/logo-airbnb.png')"}}></div>
-						<form>
+						<form onSubmit={this.submitLogin} >
 							<div className="group">
 								<label>Email</label>
-								<input type="email" />
+								<input type="email" value={this.state.email} onChange={(e) => this.sendInputToState(e, 'email')}/>
 							</div>
 							<div className="group">
 								<label>Password</label>
-								<input type="password" />
+								<input type="password" value={this.state.password} onChange={(e) => this.sendInputToState(e, 'password')} />
 							</div>
-							<Link to="/"><button className="primary">Login</button></Link>
+							<span style={{'lineHeight': '4em', 'color': 'red'}}>{this.state.errorMessage}</span>
+							<button className="primary">Login</button>
 						</form>
 						<p className="footer">
 							New to Airbnb? <Link to="/Signup">Signup</Link>
