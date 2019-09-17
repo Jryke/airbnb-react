db7e0f2b3979d776b1f83582fd0be9665b9f1d83
Jryke <rykerson@gmail.com>
2019-09-17 11:55:03 +0700

    140202

---
+++ src/components/StripeForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {CardElement, injectStripe} from 'react-stripe-elements'
+
+class StripeForm extends React.Component {
+	render() {
+		return(
+			<CardElement />
+		)
+	}
+}
+
+export default injectStripe(StripeForm)
